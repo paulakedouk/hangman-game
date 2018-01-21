b321c873c6c44ec22ef1ee1e69730abf6a204944
paulakedouk <paulakedouk@gmail.com>
2018-01-20 18:22:09 -0800

    Add new images and style the bar

---
+++ assets/javascript/game.js
@@ -0,0 +1,5 @@
+// var canvas = document.getElementById('canvas');
+// var ctx = canvas.getContext('2d');
+// var image = document.getElementById('bar');
+
+// ctx.drawImage(image, 33, 71, 104, 124, 21, 20, 87, 104);
