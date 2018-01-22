4c25a92721f5d061a76c84c7f50c27987bf32694
paulakedouk <paulakedouk@gmail.com>
2018-01-21 22:09:51 -0800

    Create showLetter() and show to the page

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,10 +1,40 @@
 // Bar body
 
 var animals = ['cat', 'dog', 'bird', 'butterfly'];
+var alphabet = 'abcdefghijklmnopqrstuvwxyz';
+var alphabetArray = alphabet.split('');
 
 var selectedIndex = 0;
 
 // Function to render name
+
+function showLetters() {
+  // Get the element
+  btnLetters = document.getElementById('btn-letters');
+  // Create a list
+  letters = document.createElement('ul');
+
+  // Create for loop to go through on each item on the array ['a', 'b', ...]
+  for (var i = 0; i < alphabetArray.length; i++) {
+    // Set id class to the ul list
+    letters.id = 'alphabet';
+
+    // Create item for the list and set an id
+    list = document.createElement('li');
+    list.id = 'letter';
+
+    // Add that list of items to the html
+    list.innerHTML = alphabet[i];
+
+    // Create a click function
+
+    // Insert an element after the last child
+    btnLetters.appendChild(letters);
+    letters.appendChild(list);
+  }
+}
+
+showLetters();
 
 function showWord() {
   selectedIndex = Math.floor(Math.random() * animals.length);
@@ -16,7 +46,14 @@ function showWord() {
     //var letters = animals[i].split('');
     document.querySelector('#empty-line').innerHTML += '_ ';
   }
+  0;
 }
+
+document.onkeyup = function(event) {
+  // When key was pressed
+  var userInput = event.key;
+  console.log(userInput);
+};
 
 // Calling functions to start the game.
 showWord();
