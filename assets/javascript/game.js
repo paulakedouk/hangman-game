abfb5f554508824eed37268e59580d0c0fe32f57
paulakedouk <paulakedouk@gmail.com>
2018-01-21 18:04:53 -0800

    Add a function to display the lines on the page

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -5,13 +5,17 @@ var animals = ['cat', 'dog', 'bird', 'butterfly'];
 var selectedIndex = 0;
 
 // Function to render name
-function showWord() {
-  //   for (var i in animals) {
-  //     console.log(animals[i].length);
-  //   }
-  selectedIndex = Math.floor(Math.random() * animals.length);
 
-  console.log(animals[selectedIndex], animals[selectedIndex].length);
+function showWord() {
+  selectedIndex = Math.floor(Math.random() * animals.length);
+  // console.log(animals[selectedIndex], animals[selectedIndex].length);
+
+  console.log(animals[selectedIndex]);
+
+  for (var i in animals[selectedIndex]) {
+    //var letters = animals[i].split('');
+    document.querySelector('#empty-line').innerHTML += '_ ';
+  }
 }
 
 // Calling functions to start the game.
